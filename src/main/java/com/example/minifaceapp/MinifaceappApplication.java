f3321224437de94f356f208e1c5913ac86d5304d
DarjanCrncic <darjancr@gmail.com>
2021-04-24 12:32:20 +0200

    Initial commit.

---
+++ src/main/java/com/example/minifaceapp/MinifaceappApplication.java
@@ -0,0 +1,13 @@
+package com.example.minifaceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinifaceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinifaceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minifaceapp/MinifaceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minifaceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinifaceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
